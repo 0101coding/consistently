1bb2710776a04a469c53ecd763438cd4d61f85b4
Mayowa <mayowadpastevans@yahoo.co.uk>
2022-03-10 20:23:33 +0100

    Cleaned up the functions and made some changes

--- contracts/Consistenly.sol
+++ contracts/Consistenly.sol
@@ -3,15 +3,19 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Consistently {
+ 
+
     mapping(address => bool) public userAddresses;
     address public owner;
     uint256 public timestarts;
     
     struct Intention {
+        uint8 noOfDays;
         uint8 defaulted; // Only 4 defaults are permitted
         uint256 timeRegistered;
         uint256 weiDeposited;
-        bytes32 habitHash;
+        uint256 weiBalance;
+        string habit;
         address userAddress;
         bool active;
         
@@ -19,6 +23,7 @@ contract Consistently {
     Intention[] public intentions;
     constructor() {
         owner = msg.sender;
+        //TODO: Mint all the Habit Tokens
     }
 
 
@@ -26,14 +31,17 @@ contract Consistently {
     mapping (address => uint) public userIntentions;
     mapping (address => uint) public lastCheckIn;
     mapping (address => uint) public nextCheckIn;
+    mapping (address => uint8) public checkInCount;
 
-    event IntentionRegistered(address userAddress, uint256 weiDeposited, bytes32 habitHash, uint256 timeRegistered, bool active);
+    event IntentionRegistered(address userAddress, uint256 weiDeposited, string habit, uint256 timeRegistered, bool active);
     event CheckedIn(address userAddress, uint256 lastCheckInTime);
     // Allow the user to register what habit they are trying to form or break
-    function register(bytes32 _habithash) external payable {
+    function register(string memory _habit, uint8 _days) external payable {
+        require(_days >= 21, "Minimum of 21 days Acceptable");
+        require(_days <= 255, "Maximum of 255 days Acceptable");
         uint256 currentTime = block.timestamp;
         
-        Intention memory intent = Intention(0, currentTime, msg.value, _habithash, msg.sender, true);
+        Intention memory intent = Intention(_days, 0, currentTime, msg.value, msg.value, _habit, msg.sender, true);
         intentions.push(intent);
         // Add to list of Addresses. if not already there
         userAddresses[msg.sender] = true;
@@ -46,8 +54,9 @@ contract Consistently {
         lastCheckIn[msg.sender] = currentTime;
         nextCheckIn[msg.sender] = currentTime * 86400; // Next CheckIn Time is in 24 Hours
 
-        emit IntentionRegistered(msg.sender, msg.value, _habithash, currentTime, true );
+        emit IntentionRegistered(msg.sender, msg.value, _habit, currentTime, true );
     }
+ 
 
     function checkin() external 
         onlyRegisteredUsers(msg.sender) 
@@ -63,10 +72,13 @@ contract Consistently {
             penalizeUserForLateCheckIn(msg.sender);
         } 
 
+        
+        //TODO: Transfer the Token to the User Address
+
+        checkInCount[msg.sender]++;
         lastCheckIn[msg.sender] = currentTime;
         nextCheckIn[msg.sender] = currentTime * 86400; // Next CheckIn Time is in 24 Hours
         emit CheckedIn(msg.sender, currentTime);
-        
     }
 
 
@@ -75,9 +87,10 @@ contract Consistently {
 
         uint intentionId = userIntentions[_userAddress] - 1;
         Intention storage intention = intentions[intentionId];
-
-        intention.weiDeposited -= intention.weiDeposited / 4;
-       
+        // This line is buggy
+        intention.weiBalance = intention.weiDeposited / 4;
+        // increment the defaulted
+        intention.defaulted++;
         if (intention.defaulted == 4){
             intention.active == false;
             userIntentions[_userAddress] = 0; // reset the userIntention
@@ -85,7 +98,23 @@ contract Consistently {
         }
     }
 
-    // Explore TimeBased Events. Set the Next Max Min App
+
+
+
+    //TODO: Find a way to penalize the user if they do not checkin for a number of days
+    // Set the User's Last Checkin Time and User's Next Check In
+    // Emit an event and notify the User that He is penalized.
+
+
+    //TODO Have a function to deposit the contract balance into AAVE when it is more than 1 ETH
+    // This function can be run by Chain link keeper
+
+    //TODO Have a redeem function to allow the User redeem Eth balance in the Contract.
+    // Must not be able to redeem unless quest is completed
+    // Must burn the habit token once redeemed
+    // Mint an NFT to the User
+
+    //TODO: Have a external view function to return the user balance, Deposited Wei, Days Remaining, Habit
 
     modifier onlyRegisteredUsers(address _userAddress) {
         require(userAddresses[_userAddress]);

8ce6a6767d5ea4fc502189e1137d75b97e0da1ce
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-03-16 12:50:51 -0400

    Contract upgrade, friends ready

--- contracts/Social.sol
+++ contracts/Social.sol
@@ -5,55 +5,63 @@ import "hardhat/console.sol";
 
 contract Social {
 
+    enum Friend {No, Pending, Yes}
+
     mapping(address => bool) public isMember;
-    mapping(address => mapping(address => bool)) public areFriends; // Member => mapping(Guardian => bool);
-    // mapping(address => mapping(address => bool)) public guardianOf; // Guardian => mapping(Member => bool);
+    mapping(address => mapping(address => Friend)) public areFriends;
 
     modifier OnlyMembers {
-        require(isMember[msg.sender], "not a member");
+        require(isMember[msg.sender], "Not a member");
         _;
     }
 
     modifier NotSelf(address addr) {
-        require(!(addr == msg.sender), "Can't guard yourself");
+        require(!(addr == msg.sender), "Can't friend yourself");
         _;
     }
 
-    modifier OnlyGuardians(address member) {
-        require(areFriends[member][msg.sender], "Not a guardian");
+    modifier OnlyFriends(address friend) {
+        require(areFriends[msg.sender][friend] == Friend.Yes, "Not friends");
         _;
     }
 
-    modifier OnlyAuthorized(address member) {
-        require(isMember[msg.sender] || areFriends[member][msg.sender], "Not a member and not friends");
+    modifier OnlyAuthorized(address friend) {
+        require(isMember[msg.sender] || areFriends[msg.sender][friend] == Friend.Yes, "Not a member and not friends");
         _;
     }
 
+    //Register membership.
     function register() public {
         require(!isMember[msg.sender], "You're already a member");
         isMember[msg.sender] = true;
     }
 
-    function registerGuardian(address addr) public OnlyMembers NotSelf(addr) {
-        areFriends[msg.sender][addr] = true;
+    //Register/Remove one or multiple friends.
+    function registerFriend(address addr) public OnlyMembers NotSelf(addr) {
+        if(areFriends[addr][msg.sender] == Friend.No) {
+        areFriends[msg.sender][addr] = Friend.Pending;
+        } else {
+            areFriends[msg.sender][addr] = Friend.Yes;
+            areFriends[addr][msg.sender] = Friend.Yes;
+        }
     }
 
-    function registerGuardians(address[] calldata addrs) public {
+    function registerFriends(address[] calldata addrs) public {
         register();
         for(uint i = 0; i < addrs.length; i++) {
-            registerGuardian(addrs[i]);
+            registerFriend(addrs[i]);
         }
     }
 
-    function removeGuardian(address addr) public OnlyMembers {
-        areFriends[msg.sender][addr] = false;
+    function removeFriend(address addr) public OnlyMembers {
+        areFriends[msg.sender][addr] = Friend.No;
+        areFriends[addr][msg.sender] = Friend.No;
     }
 
-    function removeGuardians(address[] calldata addrs) public {
+    function removeFriends(address[] calldata addrs) public {
         for(uint i = 0; i < addrs.length; i++) {
-            removeGuardian(addrs[i]);
+            removeFriend(addrs[i]);
         }
     }    
-
 
 }
